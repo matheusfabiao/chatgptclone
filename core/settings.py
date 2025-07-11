283f944db18f092834c10b12de6f6849bba7d78d
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-07-11 17:17:34 -0300

    add CSRF_TRUSTED_ORIGINS config in settings.py

--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,7 @@ DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').strip().split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').strip().split(',')
 
 # Application definition
 
